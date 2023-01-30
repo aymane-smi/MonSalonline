e8ab76736bca65e33bbea1cddec219ad16313111
aymane-smi <aymanebel2@outlook.fr>
2023-01-30 15:44:14 +0100

    creating models

---
+++ backend/app/models/Client.php
@@ -0,0 +1,45 @@
+<?php
+    class Client{
+        private $db;
+
+        public function __construct(){
+            $this->db = new DB();
+        }
+
+          /**
+           * add client by using his informations
+           * Summary of addClient
+           * @param mixed $fname
+           * @param mixed $lname
+           * @param mixed $email
+           * @param mixed $phone
+           * @param mixed $token
+           * @return void
+           */
+
+        public function addClient($fname, $lname, $email, $phone, $token){
+            $this->db->query("INSERT INTO client(fname, lname, email, phone, token) VALUES(:fname, :lname, :email, :phone, :token)");
+            $this->db->bind(":fname", $fname);
+            $this->db->bind(":lname", $lname);
+            $this->db->bind(":email", $email);
+            $this->db->bind(":phone", $phone);
+            $this->db->bind(":token", $token);
+            $this->db->execute();
+        }
+
+          /**
+           * edit client token by using his id
+           * Summary of editToken
+           * @param mixed $id
+           * @param mixed $token
+           * @return void
+           */
+
+        public function editToken($id, $token){
+            $this->db->query("UPDATE client SET token = :token WHERE id = :id");
+            $this->db->bind(":token", $token);
+            $this->db->bind(":id", $id);
+            $this->db->execute();
+        }
+    }
+?>
---
+++ backend/app/models/Schedule.php
@@ -0,0 +1,50 @@
+<?php
+    class Schedule{
+        private $db;
+
+        public function __construct(){
+            $this->db = new DB();
+        }
+
+        /**
+         * add appointment of the client by given the date and hour
+         * @param mixed $id_client
+         * @param mixed $date
+         * @param mixed $hour
+         * @return void
+         */
+        public function add($id_client, $date, $hour){
+            $this->db->query("INSERT INTO appointment(id_client, date, hour) VALUE($id_client, $date, $hour)");
+            $this->db->bind(":id_client", $id_client);
+            $this->db->bind(":date", $date);
+            $this->db->bind(":hour", $hour);
+            $this->db->execute();
+        }
+
+        /**
+         * delete appointment by id
+         * @param mixed $id
+         * @return void
+         */
+        public function deleteById($id){
+            $this->db->query("DELETE FROM appointment WHERE id = :id");
+            $this->db->bind(":id", $id);
+            $this->db->execute();
+        }
+
+        /**
+         * edit Appointment by giving the id of the the appointment and the new date and hour
+         * @param mixed $id
+         * @param mixed $date
+         * @param mixed $hour
+         * @return void
+         */
+        public function edit($id, $date, $hour){
+        $this->db->query("UPDATE appointment SET date = :date, hour = :hour WHERE id = :id");
+        $this->db->bind(":date", $date);
+        $this->db->bind(":hour", $hour);
+        $this->db->bind(":id", $id);
+        $this->db->execute();
+        } 
+    }
+?>
