69c11887968d112bbd809a0f7d637ba46e8c4bb6
aymane-smi <aymanebel2@outlook.fr>
2023-01-31 15:19:48 +0100

    adding routing to the application

---
+++ frontend/src/components/Navbar.vue
@@ -0,0 +1,8 @@
+<template>
+    <nav class="px-4 w-screen border-[1px] border-grey-500 h-[60px] flex justify-between items-center shadow-md fixed">
+        <p>MonSalonLine</p>
+        <p></p>
+    </nav>
+</template>
+<script lang="ts" setup>
+</script>
