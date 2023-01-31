f275be68863eb60b94f0e02303ac32fbc36a0018
aymane-smi <aymanebel2@outlook.fr>
2023-01-31 12:24:41 +0100

    creating the landing page with tailwind for vue

---
+++ frontend/src/components/Home.vue
@@ -0,0 +1,28 @@
+<template>
+    <nav class="font-tako mt-3 border-t-[4px] border-white mx-6 text-white pt-6 px-3 flex justify-between items-center">
+       <div class="flex flex-col text-[20px]">
+            <a href="/">Home</a>
+            <a href="/login">Login</a>
+       </div>
+       <p class="text-[30px]">MonSalonLine</p>
+       <div class="font-thin">
+        <p>address</p>
+        <p>1234567890</p>
+        <p>email@test.com</p>
+       </div> 
+    </nav>
+    <main class="mt-[40px] w-full">
+        <div class="w-full text-white flex justify-start items-start gap-[50px] pl-[100px] font-tako leading-[120px]">
+            <span class="text-[60px] ">/01</span>
+            <p class="w-[27%] text-[100px] align-text-top">BARBERING IS WHAT WE DO</p>
+        </div>
+
+        <div class="flex pl-[200px] gap-5 font-semibold font-tako">
+            <a class="bg-[#efefef] rounded-[20px] p-4 text-black">make an appointment</a>
+            <a class="border-[#efefef] border-[2px] rounded-[20px] p-4 text-[#efefef]">signup</a>
+        </div>
+    </main>
+</template>
+<script setup>
+</script>
+<style scoped></style>
